9298ec8f3ea2c36cc856afe385af87314233f839
Midhun P <midhunnair2004@gmail.com>
2026-02-25 09:20:51 +0530

    Update user-data.sh For Making Some Modifications

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-
+# Update system
 dnf update -y
 
+# Install Docker
 dnf install docker -y
-systemctl start dokcer
-systemctl enable dokcer
+
+# Start Docker service
+systemctl start docker
+systemctl enable docker
+
+# Allow ec2-user to run Docker
 usermod -aG docker ec2-user
 
+# Give Docker few seconds to fully start
+sleep 10
 
-docker run -d -p 80:80 nginx
+# Pull latest image from DockerHub
+docker pull midhun07/terraform-docker-ec2-autodeploy:latest
+
+# Run container
+docker run -d -p 80:80 --restart unless-stopped --name webapp midhun07/terraform-docker-ec2-autodeploy:latest
